c95ffbfcf9e40890c926634a7ad2e31fe755300b
Ayman Bagabas <ayman.bagabas@gmail.com>
2021-03-15 17:27:13 -0400

    Add telescope live grep keymap

--- .config/nvim/lua/telescope-nvim/lua.lua
+++ .config/nvim/lua/telescope-nvim/lua.lua
@@ -69,6 +69,7 @@ vim.api.nvim_set_keymap(
     opt
 )
 vim.api.nvim_set_keymap("n", "<Leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fg", [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>fh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>fo", [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], opt)
 vim.api.nvim_set_keymap("n", "<Leader>fm", [[<Cmd> Neoformat<CR>]], opt)
