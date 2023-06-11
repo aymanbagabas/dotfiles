4d6627f052c8d45a28429f27e2b7aa337d0c70c8
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-06-11 18:51:40 -0400

    fix: create nvim directory

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Create nvim config directory
+if ! $DRY_RUN; then
+	mkdir -p ~/.config/nvim
+fi
+
 link_file lua ~/.config/nvim/lua
 link_file init.lua ~/.config/nvim/init.lua
 link_file lazy-lock.json ~/.config/nvim/lazy-lock.json
