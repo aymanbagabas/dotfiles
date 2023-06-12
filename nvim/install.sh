7d7ecd73aa9950ca2e9700e21bc83f72c1e0e846
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-06-12 01:09:24 -0400

    fix(nvim): don't recreate dir if exist

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,7 +5,7 @@ if ! $DRY_RUN; then
 	mkdir -p ~/.config/nvim
 fi
 
-link_file lua ~/.config/nvim/lua
+link_file lua ~/.config/nvim/
 link_file init.lua ~/.config/nvim/init.lua
 link_file lazy-lock.json ~/.config/nvim/lazy-lock.json
 link_file neoconf.json ~/.config/nvim/neoconf.json
