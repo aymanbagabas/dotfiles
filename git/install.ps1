04fc66750095a5740f73fd3e62eb85fdb8ef02f3
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-09-23 18:05:03 -0400

    fit(git): gpg/ssh command paths on windows

--- git/install.ps1
+++ git/install.ps1
@@ -4,5 +4,11 @@ Link-File "$PSScriptRoot\ignore" "$Env:USERPROFILE\.gitignore"
 Write-Host "Creating global git config..."
 if (!$DRY_RUN) {
 	Templatize "$PSScriptRoot\config" | Invoke-Expression
-	git config --global gpg.program "C:\Program Files (x86)\GnuPG\bin\gpg.exe"
+
+	# XXX: We're intentionally using forward slashes (/) here because git
+	# doesn't like backslashes in paths.
+	git config --global gpg.program "C:/Program Files (x86)/GnuPG/bin/gpg.exe"
+
+	# Use Windows OpenSSH
+	git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
 }
