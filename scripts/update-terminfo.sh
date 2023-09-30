274b351a59c064decea18653f1794164008b1c45
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-09-30 17:29:08 -0400

    fix(scripts): add alacritty to update-terminfo

--- scripts/update-terminfo.sh
+++ scripts/update-terminfo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # This script will update the terminfo database with the latest version of
-# terminfo definitions from ncurses for tmux, and alacrity-direct.
+# terminfo definitions from ncurses for tmux, and alacrity.
 # See: https://gist.github.com/bbqtd/a4ac060d6f6b9ea6fe3aabe735aa9d95
 
 set -e
@@ -15,7 +15,7 @@ curl -sLO https://invisible-island.net/datafiles/current/terminfo.src.gz && gunz
 
 # Update database
 echo "Updating terminfo database..."
-/usr/bin/tic -xe alacritty-direct,tmux-256color terminfo.src
+/usr/bin/tic -xe alacritty,alacritty-direct,tmux-256color terminfo.src
 
 # Cleanup
 echo "Cleaning up..."
