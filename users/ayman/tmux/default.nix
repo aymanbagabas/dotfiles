dd6ee2e9b978f250a1d276266e4c481316ebdc92
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-05-02 11:31:28 -0400

    fix(nix): don't use hw tmux settings

--- users/ayman/tmux/default.nix
+++ users/ayman/tmux/default.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.tmux = {
-      enable = true;
-  };
+  home.packages = with pkgs; [
+    tmux
+  ];
 
   # Home Manager inserts settings we don't want, so just symlink our config.
   home.file.".tmux.conf".source = ./tmux.conf;
