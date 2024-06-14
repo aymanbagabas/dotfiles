0ccd91d476b9a698bd3403a0306f80e9ecc4d3f1
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-06-13 20:05:50 -0400

    fix(nvim): disable cmp ghost text

--- users/ayman/neovim/config/plugin/completion.lua
+++ users/ayman/neovim/config/plugin/completion.lua
@@ -173,9 +173,7 @@ local opts = {
     end,
   },
   experimental = {
-    ghost_text = {
-      hl_group = "CmpGhostText",
-    },
+    ghost_text = false, -- { hl_group = "CmpGhostText" },
   },
   sorting = defaults.sorting,
 }
