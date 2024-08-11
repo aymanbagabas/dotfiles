60cce939999c7520d7ec365717f8d595285b7ea6
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-08-11 04:28:09 -0400

    fix: hm release version

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nur.url = "github:nix-community/NUR";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
