aymanbagabas/dotfiles

commit f38107b275385650d551b27b8f32a6499e1f1a55

fix(nvim): properly run LspDetach event

modules/neovim/config/lua/user/lsp.lua
@@ -46,20 +46,10 @@ function M.get_clients(opts)
   return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
 end
 
----@param path string
----@return string|nil
-local realpath = function(path)
-  if path == "" or path == nil then
-    return nil
-  end
-  path = vim.uv.fs_realpath(path) or path
-  return M.norm(path)
-end
-
 local rename_file = function()
   local buf = vim.api.nvim_get_current_buf()
-  local old = assert(realpath(vim.api.nvim_buf_get_name(buf)))
-  local root = assert(realpath(Root.get_root({ normalize = true })))
+  local old = assert(Root.realpath(vim.api.nvim_buf_get_name(buf)))
+  local root = assert(Root.realpath(Root.get_root({ normalize = true })))
   assert(old:find(root, 1, true) == 1, "File not in project root")
 
   local extra = old:sub(#root + 2)
@@ -289,34 +279,33 @@ M.setup = function()
           })
         end
       end
-    end,
-  })
 
-  vim.api.nvim_create_autocmd("LspDetach", {
-    group = vim.api.nvim_create_augroup("LspDetachGroup", { clear = true }),
-    callback = function(args)
-      local bufnr = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client == nil then
-        return
-      end
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vim.api.nvim_create_augroup("LspDetachGroup_" .. bufnr, { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client == nil then
+            return
+          end
 
-      -- don't trigger on invalid buffers
-      if not vim.api.nvim_buf_is_valid(bufnr) then
-        return
-      end
-      -- don't trigger on non-listed buffers
-      if not vim.bo[bufnr].buflisted then
-        return
-      end
-      -- don't trigger on nofile buffers
-      if vim.bo[bufnr].buftype == "nofile" then
-        return
-      end
+          -- don't trigger on invalid buffers
+          if not vim.api.nvim_buf_is_valid(bufnr) then
+            return
+          end
+          -- don't trigger on non-listed buffers
+          if not vim.bo[bufnr].buflisted then
+            return
+          end
+          -- don't trigger on nofile buffers
+          if vim.bo[bufnr].buftype == "nofile" then
+            return
+          end
 
-      if client.supports_method(ms.textDocument_codeLens) then
-        vim.lsp.codelens.clear(client.id, bufnr)
-      end
+          if client.supports_method(ms.textDocument_codeLens) then
+            vim.lsp.codelens.clear(client.id, bufnr)
+          end
+        end,
+      })
     end,
   })
 end

modules/neovim/config/lua/user/root.lua
@@ -11,6 +11,32 @@ local M = {}
 ---@type RootSpec[]
 M.spec = { "lsp", { ".git", "lua" }, "cwd" }
 
+---@type table<number, string>
+M.cache = {}
+
+M.detectors = {}
+
+function M.detectors.pattern(buf, patterns)
+  patterns = type(patterns) == "string" and { patterns } or patterns
+  local path = M.bufpath(buf) or vim.uv.cwd()
+  local pattern = vim.fs.find(function(name)
+    for _, p in ipairs(patterns) do
+      if name == p then
+        return true
+      end
+      if p:sub(1, 1) == "*" and name:find(vim.pesc(p:sub(2)) .. "$") then
+        return true
+      end
+    end
+    return false
+  end, { path = path, upward = true })[1]
+  return pattern and { vim.fs.dirname(pattern) } or {}
+end
+
+function M.bufpath(buf)
+  return M.realpath(vim.api.nvim_buf_get_name(assert(buf)))
+end
+
 function M.is_win()
   return vim.uv.os_uname().sysname:find("Windows") ~= nil
 end
@@ -29,6 +55,29 @@ function M.norm(path)
   end
   path = path:gsub("\\", "/"):gsub("/+", "/")
   return path:sub(-1) == "/" and path:sub(1, -2) or path
+end
+
+---@param spec RootSpec
+---@return RootFn
+function M.resolve(spec)
+  if M.detectors[spec] then
+    return M.detectors[spec]
+  elseif type(spec) == "function" then
+    return spec
+  end
+  return function(buf)
+    return M.detectors.pattern(buf, spec)
+  end
+end
+
+---@param path string
+---@return string|nil
+function M.realpath(path)
+  if path == "" or path == nil then
+    return nil
+  end
+  path = vim.uv.fs_realpath(path) or path
+  return M.norm(path)
 end
 
 ---@param opts? { buf?: number, spec?: RootSpec[], all?: boolean }

commit 34cf6f13f8255b36a10cd7b8350fbc41c923f5cd

fix(nvim): lsp: schedule detach event
Fixes: f38107b27538 (fix(nvim): properly run LspDetach event)

modules/neovim/config/lua/user/lsp.lua
@@ -280,32 +280,35 @@ M.setup = function()
         end
       end
 
-      vim.api.nvim_create_autocmd("LspDetach", {
-        group = vim.api.nvim_create_augroup("LspDetachGroup_" .. bufnr, { clear = true }),
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client == nil then
-            return
-          end
+      vim.schedule(function()
+        -- don't trigger on invalid buffers
+        if not vim.api.nvim_buf_is_valid(bufnr) then
+          return
+        end
+        -- don't trigger on non-listed buffers
+        if not vim.bo[bufnr].buflisted then
+          return
+        end
+        -- don't trigger on nofile buffers
+        if vim.bo[bufnr].buftype == "nofile" then
+          return
+        end
 
-          -- don't trigger on invalid buffers
-          if not vim.api.nvim_buf_is_valid(bufnr) then
-            return
-          end
-          -- don't trigger on non-listed buffers
-          if not vim.bo[bufnr].buflisted then
-            return
-          end
-          -- don't trigger on nofile buffers
-          if vim.bo[bufnr].buftype == "nofile" then
-            return
-          end
+        vim.api.nvim_create_autocmd("LspDetach", {
+          buffer = bufnr,
+          group = vim.api.nvim_create_augroup("LspDetachGroup_" .. bufnr, { clear = true }),
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client == nil then
+              return
+            end
 
-          if client.supports_method(ms.textDocument_codeLens) then
-            vim.lsp.codelens.clear(client.id, bufnr)
-          end
-        end,
-      })
+            if client.supports_method(ms.textDocument_codeLens) then
+              vim.lsp.codelens.clear(client.id, bufnr)
+            end
+          end,
+        })
+      end)
     end,
   })
 end