12714d579963250277df740c8193189427b3863a
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-08-14 11:12:28 -0400

    fix(nvim): which-key: deprecations

--- modules/neovim/config/plugin/mini-ai.lua
+++ modules/neovim/config/plugin/mini-ai.lua
@@ -1,82 +1,145 @@
+---@alias Mini.ai.loc {line:number, col:number}
+---@alias Mini.ai.region {from:Mini.ai.loc, to:Mini.ai.loc}
+
+-- Mini.ai indent text object
+-- For "a", it will include the non-whitespace line surrounding the indent block.
+-- "a" is line-wise, "i" is character-wise.
+local ai_indent = function(ai_type)
+  local spaces = (" "):rep(vim.o.tabstop)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local indents = {} ---@type {line: number, indent: number, text: string}[]
+
+  for l, line in ipairs(lines) do
+    if not line:find("^%s*$") then
+      indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), text = line }
+    end
+  end
+
+  local ret = {} ---@type (Mini.ai.region | {indent: number})[]
+
+  for i = 1, #indents do
+    if i == 1 or indents[i - 1].indent < indents[i].indent then
+      local from, to = i, i
+      for j = i + 1, #indents do
+        if indents[j].indent < indents[i].indent then
+          break
+        end
+        to = j
+      end
+      from = ai_type == "a" and from > 1 and from - 1 or from
+      to = ai_type == "a" and to < #indents and to + 1 or to
+      ret[#ret + 1] = {
+        indent = indents[i].indent,
+        from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
+        to = { line = indents[to].line, col = #indents[to].text },
+      }
+    end
+  end
+
+  return ret
+end
+
+-- taken from MiniExtra.gen_ai_spec.buffer
+local ai_buffer = function(ai_type)
+  local start_line, end_line = 1, vim.fn.line("$")
+  if ai_type == "i" then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
+
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+end
+
+-- register all text objects with which-key
+---@param opts table
+local ai_whichkey = function(opts)
+  local objects = {
+    { " ", desc = "whitespace" },
+    { '"', desc = '" string' },
+    { "'", desc = "' string" },
+    { "(", desc = "() block" },
+    { ")", desc = "() block with ws" },
+    { "<", desc = "<> block" },
+    { ">", desc = "<> block with ws" },
+    { "?", desc = "user prompt" },
+    { "U", desc = "use/call without dot" },
+    { "[", desc = "[] block" },
+    { "]", desc = "[] block with ws" },
+    { "_", desc = "underscore" },
+    { "`", desc = "` string" },
+    { "a", desc = "argument" },
+    { "b", desc = ")]} block" },
+    { "c", desc = "class" },
+    { "d", desc = "digit(s)" },
+    { "e", desc = "CamelCase / snake_case" },
+    { "f", desc = "function" },
+    { "g", desc = "entire file" },
+    { "i", desc = "indent" },
+    { "o", desc = "block, conditional, loop" },
+    { "q", desc = "quote `\"'" },
+    { "t", desc = "tag" },
+    { "u", desc = "use/call" },
+    { "{", desc = "{} block" },
+    { "}", desc = "{} with ws" },
+  }
+
+  local ret = { mode = { "o", "x" } }
+  ---@type table<string, string>
+  local mappings = vim.tbl_extend("force", {}, {
+    around = "a",
+    inside = "i",
+    around_next = "an",
+    inside_next = "in",
+    around_last = "al",
+    inside_last = "il",
+  }, opts.mappings or {})
+  mappings.goto_left = nil
+  mappings.goto_right = nil
+
+  for name, prefix in pairs(mappings) do
+    name = name:gsub("^around_", ""):gsub("^inside_", "")
+    ret[#ret + 1] = { prefix, group = name }
+    for _, obj in ipairs(objects) do
+      local desc = obj.desc
+      if prefix:sub(1, 1) == "i" then
+        desc = desc:gsub(" with ws", "")
+      end
+      ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+    end
+  end
+  require("which-key").add(ret, { notify = false })
+end
+
 local ai = require("mini.ai")
 local opts = {
   n_lines = 500,
   custom_textobjects = {
-    o = ai.gen_spec.treesitter({
+    o = ai.gen_spec.treesitter({ -- code block
       a = { "@block.outer", "@conditional.outer", "@loop.outer" },
       i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-    }, {}),
-    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+    }),
+    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+    t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
     d = { "%f[%d]%d+" }, -- digits
     e = { -- Word with case
-      {
-        "%u[%l%d]+%f[^%l%d]",
-        "%f[%S][%l%d]+%f[^%l%d]",
-        "%f[%P][%l%d]+%f[^%l%d]",
-        "^[%l%d]+%f[^%l%d]",
-      },
+      { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
       "^().*()$",
     },
-    g = function() -- Whole buffer, similar to `gg` and 'G' motion
-      local from = { line = 1, col = 1 }
-      local to = {
-        line = vim.fn.line("$"),
-        col = math.max(vim.fn.getline("$"):len(), 1),
-      }
-      return { from = from, to = to }
-    end,
+    i = ai_indent, -- indent
+    g = ai_buffer, -- buffer
     u = ai.gen_spec.function_call(), -- u for "Usage"
     U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
   },
 }
 
 ai.setup(opts)
-
----@type table<string, string|table>
-local i = {
-  [" "] = "Whitespace",
-  ['"'] = 'Balanced "',
-  ["'"] = "Balanced '",
-  ["`"] = "Balanced `",
-  ["("] = "Balanced (",
-  [")"] = "Balanced ) including white-space",
-  [">"] = "Balanced > including white-space",
-  ["<lt>"] = "Balanced <",
-  ["]"] = "Balanced ] including white-space",
-  ["["] = "Balanced [",
-  ["}"] = "Balanced } including white-space",
-  ["{"] = "Balanced {",
-  ["?"] = "User Prompt",
-  _ = "Underscore",
-  a = "Argument",
-  b = "Balanced ), ], }",
-  c = "Class",
-  d = "Digit(s)",
-  e = "Word in CamelCase & snake_case",
-  f = "Function",
-  g = "Entire file",
-  o = "Block, conditional, loop",
-  q = "Quote `, \", '",
-  t = "Tag",
-  u = "Use/call function & method",
-  U = "Use/call without dot in name",
-}
-local a = vim.deepcopy(i)
-for k, v in pairs(a) do
-  ---@diagnostic disable-next-line: param-type-mismatch
-  a[k] = v:gsub(" including.*", "")
-end
-
-local ic = vim.deepcopy(i)
-local ac = vim.deepcopy(a)
-for key, name in pairs({ n = "Next", l = "Last" }) do
-  i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
-  a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
-end
-require("which-key").register({
-  mode = { "o", "x" },
-  i = i,
-  a = a,
-})
+vim.schedule(function()
+  ai_whichkey(opts)
+end)
--- modules/neovim/config/plugin/which-key.lua
+++ modules/neovim/config/plugin/which-key.lua
@@ -1,26 +1,53 @@
 local opts = {
   plugins = { spelling = true },
-  defaults = {
-    mode = { "n", "v" },
-    ["g"] = { name = "+goto" },
-    ["gs"] = { name = "+surround" },
-    ["]"] = { name = "+next" },
-    ["["] = { name = "+prev" },
-    ["<leader><tab>"] = { name = "+tabs" },
-    ["<leader>b"] = { name = "+buffer" },
-    ["<leader>c"] = { name = "+code" },
-    ["<leader>l"] = { name = "+lsp" },
-    ["<leader>f"] = { name = "+file/find" },
-    ["<leader>g"] = { name = "+git" },
-    ["<leader>gh"] = { name = "+hunks" },
-    ["<leader>q"] = { name = "+quit/session" },
-    ["<leader>s"] = { name = "+search" },
-    ["<leader>u"] = { name = "+ui" },
-    ["<leader>w"] = { name = "+windows" },
-    ["<leader>x"] = { name = "+diagnostics/quickfix" },
+  spec = {
+    {
+      mode = { "n", "v" },
+
+      { "<leader><tab>", group = "tabs" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "file/find" },
+      { "<leader>g", group = "git" },
+      { "<leader>gh", group = "hunks" },
+      { "<leader>q", group = "quit/session" },
+      { "<leader>s", group = "search" },
+      { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+      { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+      { "[", group = "prev" },
+      { "]", group = "next" },
+      { "g", group = "goto" },
+      { "gs", group = "surround" },
+      { "z", group = "fold" },
+      {
+        "<leader>b",
+        group = "buffer",
+        expand = function()
+          return require("which-key.extras").expand.buf()
+        end,
+      },
+      {
+        "<leader>w",
+        group = "windows",
+        proxy = "<c-w>",
+        expand = function()
+          return require("which-key.extras").expand.win()
+        end,
+      },
+      -- better descriptions
+      { "gx", desc = "Open with system app" },
+
+      { "<leader>l", group = "lsp" },
+    },
   },
 }
 
 local wk = require("which-key")
 wk.setup(opts)
-wk.register(opts.defaults)
+
+vim.keymap.set("n", "<leader>?", function()
+  wk.show({ global = false })
+end, { noremap = true, silent = true, desc = "Buffer Keymaps (which-key)" })
+
+vim.keymap.set("n", "<c-w><space>", function()
+  wk.show({ keys = "<c-w>", loop = true })
+end, { noremap = true, silent = true, desc = "Window Hydra Mode (which-key)" })
