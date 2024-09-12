84a4bbb7167d87e27092cb838a9061294ea82146
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-09-12 10:39:47 -0400

    feat: add gdc git diff choose script

--- modules/neovim/config/init.lua
+++ modules/neovim/config/init.lua
@@ -3,3 +3,7 @@ require("config.keymaps")
 
 require("user.autocommands")
 require("user.lsp")
+
+-- Add local plugin rtp path.
+vim.opt.runtimepath:append("~/.local/share/nvim/site/pack/*/start/*")
+vim.opt.runtimepath:append("~/.local/share/nvim/site/plugin/*")
