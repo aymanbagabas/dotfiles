2bf536630268e9c50a3e470136fc742e4f56d669
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-09-15 01:20:47 -0400

    feat: enable vaapi on host media

--- hosts/media/configuration.nix
+++ hosts/media/configuration.nix
@@ -56,6 +56,23 @@ in {
     };
   };
 
+  # enable vaapi on OS-level
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
