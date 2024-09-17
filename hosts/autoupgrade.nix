00b58406d81bdda363929a9d13d43f8ab38f15e7
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-09-17 18:17:32 -0400

    fix: autoupgrade: add flags to only update nixpkgs

--- hosts/autoupgrade.nix
+++ hosts/autoupgrade.nix
@@ -5,5 +5,10 @@
     allowReboot = true;
     dates = "03:00";
     flake = "github:aymanbagabas/dotfiles";
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
   };
 }
