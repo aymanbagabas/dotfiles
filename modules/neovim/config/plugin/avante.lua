cab7f67b4a4e2f6e27b61d738dcf49168f2585f8
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-10-11 21:23:15 -0400

    feat(avante): add configuration options

--- modules/neovim/config/plugin/avante.lua
+++ modules/neovim/config/plugin/avante.lua
@@ -1,2 +1,14 @@
 require("avante_lib").load()
-require("avante").setup({})
+require("avante").setup({
+  behaviour = {
+    auto_suggestions = false,
+  },
+  hints = { enabled = true },
+  windows = {
+    width = 30,
+    sidebar_header = {
+      align = "left",
+      rounded = false,
+    },
+  },
+})
