715c0a50d56eeb991d0f9cb4ff704a081ad98564
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-10-11 20:13:12 -0400

    feat: add .dotfiles to direnv whitelist

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,17 +1,20 @@
 { pkgs, user, ... }:
 
 let
-  homeDirectory = (if pkgs.stdenv.isDarwin then "/Users" else "/home")
-    + "/${user}";
-in {
+  homeDirectory = (if pkgs.stdenv.isDarwin then "/Users" else "/home") + "/${user}";
+in
+{
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
     config = {
-      global = { load_dotenv = true; };
+      global = {
+        load_dotenv = true;
+      };
       whitelist = {
         prefix = [
+          "${homeDirectory}/.dotfiles"
           "${homeDirectory}/Source/aymanbagabas"
           "${homeDirectory}/Source/charmbracelet"
         ];
