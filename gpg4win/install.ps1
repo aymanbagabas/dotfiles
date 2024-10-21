217f3a79f5fc04ef0c49b6d11dd2942be1c13f8f
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-10-20 21:41:12 -0700

    feat(windows): start gpg-agent on startup

--- gpg4win/install.ps1
+++ gpg4win/install.ps1
@@ -6,3 +6,6 @@ if (!$DRY_RUN) {
 }
 
 Copy-Item -Path "$PSScriptRoot\gpg-agent.conf" -Destination "$dir\gpg-agent.conf"
+
+# Run gpg-agent on startup
+Copy-Item -Path "$PSScriptRoot\gpg-connect-agent.exe.lnk" -Destination "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\gpg-connect-agent.exe.lnk"
