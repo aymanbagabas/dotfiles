86b314164b882e387aadce4e3d1e20ca96e58ee4
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-01-29 16:15:50 -0500

    fix(nvim): fallback <Tab> when there are no suggestions

--- modules/neovim/config/plugin/copilot.lua
+++ modules/neovim/config/plugin/copilot.lua
@@ -18,7 +18,7 @@ local opts = {
     keymap = {
       -- We integrate with blink.cmp to hide the menu
       -- when suggestions are visible.
-      accept = "<Tab>",
+      accept = false, -- "<Tab>", -- see below
       accept_word = "<C-l>",
       accept_line = "<C-S-l>",
       next = "<C-k>",
@@ -43,6 +43,17 @@ local opts = {
 }
 
 require("copilot").setup(opts)
+
+-- Fallback <Tab> when there are no suggestions
+vim.keymap.set("i", "<Tab>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  end
+end, {
+  silent = true,
+})
 
 -- Disable default keymaps
 -- vim.g.copilot_no_maps = true
