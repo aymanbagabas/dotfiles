3755fc057aa802474087d9f658c9d6592e4518e9
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-04-04 13:54:59 +0300

    fix(home): no need to specify home.homeDirectory on nixos

--- modules/home.nix
+++ modules/home.nix
@@ -1,11 +1,19 @@
-{ config, pkgs, isDarwin, user, ... }:
+{
+  config,
+  pkgs,
+  isDarwin,
+  user,
+  ...
+}:
 
 with pkgs.lib;
 
-let pathJoin = builtins.concatStringsSep ":";
-in {
+let
+  pathJoin = builtins.concatStringsSep ":";
+in
+{
   home.username = "${user}";
-  home.homeDirectory = (if isDarwin then "/Users" else "/home") + "/${user}";
+  home.homeDirectory = mkIf isDarwin "/Users/${user}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -16,34 +24,36 @@ in {
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  home.sessionVariables = {
-    LC_ALL = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    PROJECTS = "$HOME/Source";
-    EDITOR = "nvim";
-    VISUAL = "nvim";
+  home.sessionVariables =
+    {
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF-8";
+      PROJECTS = "$HOME/Source";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
 
-    PATH = pathJoin ([
-      "/opt/homebrew/bin"
-      "/opt/homebrew/sbin"
-      "$HOME/.npm-global/bin"
-      "$HOME/.go/bin"
-      "$HOME/.bin"
-      "$PATH"
-    ]);
+      PATH = pathJoin ([
+        "/opt/homebrew/bin"
+        "/opt/homebrew/sbin"
+        "$HOME/.npm-global/bin"
+        "$HOME/.go/bin"
+        "$HOME/.bin"
+        "$PATH"
+      ]);
 
-    # My GPG key ID
-    KEYID = "${config.programs.gpg.settings.default-key}";
+      # My GPG key ID
+      KEYID = "${config.programs.gpg.settings.default-key}";
 
-    # https://gpanders.com/blog/the-definitive-guide-to-using-tmux-256color-on-macos/
-    TERMINFO_DIRS = "$TERMINFO_DIRS:$HOME/.local/share/terminfo";
+      # https://gpanders.com/blog/the-definitive-guide-to-using-tmux-256color-on-macos/
+      TERMINFO_DIRS = "$TERMINFO_DIRS:$HOME/.local/share/terminfo";
 
-    # Disable dotnet telemetry
-    DOTNET_CLI_TELEMETRY_OPTOUT = 1;
-  } // (optionalAttrs pkgs.stdenv.isDarwin {
-    LSCOLORS = "exfxcxdxbxegedabagacad";
-    CLICOLOR = "1";
-  });
+      # Disable dotnet telemetry
+      DOTNET_CLI_TELEMETRY_OPTOUT = 1;
+    }
+    // (optionalAttrs pkgs.stdenv.isDarwin {
+      LSCOLORS = "exfxcxdxbxegedabagacad";
+      CLICOLOR = "1";
+    });
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
