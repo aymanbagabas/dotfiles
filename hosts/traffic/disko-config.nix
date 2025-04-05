37cfb29431daac5ded5989bad0d965dcab56bf0f
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-04-05 18:00:48 +0300

    fix(traffic): increase swap size to 4G

--- hosts/traffic/disko-config.nix
+++ hosts/traffic/disko-config.nix
@@ -6,7 +6,8 @@
       content = {
         type = "gpt"; # Initialize the disk with a GPT partition table
         partitions = {
-          ESP = { # Setup the EFI System Partition
+          ESP = {
+            # Setup the EFI System Partition
             type = "EF00"; # Set the partition type
             size = "256M";
             content = {
@@ -16,7 +17,8 @@
               mountOptions = [ "defaults" ];
             };
           };
-          primary = { # Setup the LVM partition
+          primary = {
+            # Setup the LVM partition
             size = "100%"; # Fill up the rest of the drive with it
             content = {
               type = "lvm_pv"; # pvcreate
@@ -26,15 +28,22 @@
         };
       };
     };
-    lvm_vg = { # vgcreate
-      vg1 = { # /dev/vg1
+    lvm_vg = {
+      # vgcreate
+      vg1 = {
+        # /dev/vg1
         type = "lvm_vg";
-        lvs = { # lvcreate
-          swap = { # Logical Volume = "swap", /dev/vg1/swap
-            size = "2G";
-            content = { type = "swap"; };
+        lvs = {
+          # lvcreate
+          swap = {
+            # Logical Volume = "swap", /dev/vg1/swap
+            size = "4G";
+            content = {
+              type = "swap";
+            };
           };
-          root = { # Logical Volume = "root", /dev/vg1/root
+          root = {
+            # Logical Volume = "root", /dev/vg1/root
             size = "100%FREE"; # Use the remaining space in the Volume Group
             content = {
               type = "filesystem";
