b53600e1de22a649aa07d6a6fbed8809c16006e2
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-06-26 17:01:44 -0400

    fix(nvim): remove diagnostics linehl

--- modules/neovim/config/plugin/lspconfig.lua
+++ modules/neovim/config/plugin/lspconfig.lua
@@ -22,10 +22,10 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.HINT] = require("icons").diagnostics.Hint,
     },
     linehl = {
-      [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-      [vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-      [vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-      [vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "",
     },
     numhl = {
       [vim.diagnostic.severity.ERROR] = "",
