f84d5b0b3c7498249f11f825941f7d498a749722
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-06-30 15:12:37 -0400

    feat(hosts): blackvm: switch to XFCE with LightDM

--- hosts/blackvm/configuration.nix
+++ hosts/blackvm/configuration.nix
@@ -58,9 +58,10 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.lightdm.enable = true;
+  services.displayManager.lightdm.greeter.enable = true;
+  services.displayManager.defaultSession = "xfce";
+  services.desktopManager.xfce.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
