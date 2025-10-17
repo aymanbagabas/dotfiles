8bc907d41ec29b2a8f6390188b095b399842d9dd
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-10-16 22:38:33 -0400

    fix: remove unused modules and packages

--- hosts/headless.nix
+++ hosts/headless.nix
@@ -2,17 +2,11 @@
 
 {
   imports = [
-    ../modules/direnv.nix
-    ../modules/gpg.nix
-    ../modules/home.nix
-    ../modules/nixpkgs.nix
     ../modules/scripts
     ../modules/tmux
-    ../modules/zsh.nix
   ];
 
   home.packages = with pkgs; [
-    git-crypt
     (with nur.repos.aymanbagabas; shcopy)
   ];
 }
