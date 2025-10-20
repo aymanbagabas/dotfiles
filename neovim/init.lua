016ba31b46e1e5797a8f1f9475cd92b0d696cace
aymanbagabas <aymanbagabas@users.noreply.github.com>
2025-10-20 16:21:08 +0000

    chore: update autogen configs

--- neovim/init.lua
+++ neovim/init.lua
@@ -74,4 +74,5 @@ vim.pack.add({
   { src = "https://github.com/tpope/vim-vinegar" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/folke/zen-mode.nvim" },
+  { src = "https://github.com/mason-org/mason.nvim" },
 })
