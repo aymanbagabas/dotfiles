2b310c334c7f8922c25f17d2de4db9898fd45626
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-10-19 21:59:23 -0400

    chore(windows): remove unused windows terminal and powershell config

--- microsoft.windowsterminal/install.ps1
+++
@@ -1,8 +0,0 @@
-$dir = "$Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-
-# Create parent directory
-if (!$DRY_RUN) {
-	New-Item -ItemType Directory -Force -Path "$dir" > $null
-}
-
-Copy-Item -Path "$PSScriptRoot\settings.json" -Destination "$dir\settings.json"
