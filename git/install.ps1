0616e37318e1fe6dd1d2acf8d569f985379a70d8
Ayman Bagabas <ayman.bagabas@gmail.com>
2026-05-04 21:37:45 -0400

    chore(gpg): remove gnugp

--- git/install.ps1
+++ git/install.ps1
@@ -1,6 +1,5 @@
 if (!$DRY_RUN) {
 	Templatize "$PSScriptRoot\gitconfig.tmpl" | Set-Content "$Env:USERPROFILE\.gitconfig" -Force
 	git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
-	git config --global gpg.program "C:/Program Files (x86)/GnuPG/bin/gpg.exe"
 }
 Link-File "$PSScriptRoot\gitignore" "$Env:USERPROFILE\.gitignore"
