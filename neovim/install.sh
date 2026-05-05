174d19dbde5c3c506b13c25abf40936856846bae
Ayman Bagabas <ayman.bagabas@gmail.com>
2026-05-04 21:50:10 -0400

    fix(nvim): link the whole config directory

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-link_file lua ~/.config/nvim/lua
-link_file plugin ~/.config/nvim/plugin
-link_file ftplugin ~/.config/nvim/ftplugin
-link_file after ~/.config/nvim/after
-link_file init.lua ~/.config/nvim/init.lua
+target="$HOME/.config/nvim"
+
+if [ -d "$target" ] && [ ! -L "$target" ]; then
+	printf "Deleting '%s'\n" "$target"
+	if ! $DRY_RUN; then
+		rm -rf "$target"
+	fi
+fi
+
+link_file . "$target"
