a5e23a9eb41fa2b66f7d28944b1f50331b2fe349
Ayman Bagabas <ayman.bagabas@gmail.com>
2026-05-19 09:14:24 -0400

    fix(nvim): remove gopls self semantic tokens advertising

--- neovim/plugin/lspconfig.lua
+++ neovim/plugin/lspconfig.lua
@@ -176,23 +176,6 @@ local lsps = {
   },
   ["gopls"] = {
     capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      -- workaround for gopls not supporting semanticTokensProvider
-      -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-      if client.name == "gopls" then
-        if not client.server_capabilities.semanticTokensProvider then
-          local semantic = client.config.capabilities.textDocument.semanticTokens
-          client.server_capabilities.semanticTokensProvider = {
-            full = true,
-            legend = {
-              tokenTypes = semantic.tokenTypes,
-              tokenModifiers = semantic.tokenModifiers,
-            },
-            range = true,
-          }
-        end
-      end
-    end,
     settings = {
       gopls = {
         gofumpt = true,
